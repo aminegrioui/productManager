329585c2496058bd8392344bc584d907a7e67771
grioui <amine23grioui@gmail.com>
2021-10-28 12:04:43 +0200

    add Product features

--- src/main/java/com/javaamine/managerproduct/ProductManager/controllers/AppController.java
+++ src/main/java/com/javaamine/managerproduct/ProductManager/controllers/AppController.java
@@ -5,7 +5,9 @@ import com.javaamine.managerproduct.ProductManager.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -21,4 +23,20 @@ public class AppController {
 
         return "index";
     }
+
+    @RequestMapping("/new")
+    public String showNewProductPage(Model model) {
+        Product product = new Product();
+        model.addAttribute("product", product);
+
+        return "new_product";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveProduct(@ModelAttribute("product") Product product) {
+        service.save(product);
+
+        return "redirect:/";
+    }
+
 }
